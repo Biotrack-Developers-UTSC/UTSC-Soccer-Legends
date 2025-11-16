4dbdf42e668ad0b61e1452c5b37a31ce4c6786a9
PedroAlbertoPM <pedroperezpm2@gmail.com>
2025-11-16 07:54:30 -0600

    Commit: Goles, Nombres de jugadores, Temporizador y animacion de gol agregadas (aun persiste el error de jugador 1).

--- scenes/game_events.gd
+++ scenes/game_events.gd
@@ -1,6 +1,9 @@
 extends Node
 
+signal ball_possesed(player_name: String)
+signal ball_released
 signal kickoff_ready
 signal kickoff_started
 signal team_reset
 signal team_scored(country_scored_on: String)
+signal score_changed
