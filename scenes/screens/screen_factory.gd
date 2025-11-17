42fee367e43f2c3e562b78818cb98e3f3c223aaf
PedroAlbertoPM <pedroperezpm2@gmail.com>
2025-11-17 16:35:34 -0600

    Commit: Juego base (sin Quiz) terminado.

--- scenes/screens/screen_factory.gd
+++ scenes/screens/screen_factory.gd
@@ -5,10 +5,10 @@ var screens : Dictionary
 func _init() -> void:
 	screens = { 
 		SoccerGame.ScreenType.MAIN_MENU: preload("res://scenes/screens/main_menu/main_menu_screen.tscn"),
-		#SoccerGame.ScreenType.TOURNAMENT: preload(),
-		#SoccerGame.ScreenType.QUIZ: preload(),
+		SoccerGame.ScreenType.TOURNAMENT: preload("res://scenes/screens/tournament/tournament_screen.tscn"),
 		SoccerGame.ScreenType.TEAM_SELECTION: preload("res://scenes/screens/team_selection/team_selection_screen.tscn"),
 		SoccerGame.ScreenType.IN_GAME: preload("res://scenes/screens/world/world_screen.tscn"),
+		#SoccerGame.ScreenType.QUIZ: preload(),
 	}
 
 func get_fresh_screen(screen: SoccerGame.ScreenType) -> Screen:
